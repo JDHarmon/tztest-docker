786a1a4ad52236c3088a97ae41d5664fbb637526
Jason Harmon <jason@jdharmon.com>
2017-05-21 14:13:13 -0400

    Use interpolated strings.

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Local Time Zone Info\n====================");
-        Console.WriteLine("Id:\t\t" + TimeZoneInfo.Local.Id);
-        Console.WriteLine("Display Name:\t" + TimeZoneInfo.Local.DisplayName);
-        Console.WriteLine("StandardName:\t" + TimeZoneInfo.Local.StandardName);
-        Console.WriteLine("DaylightName:\t" + TimeZoneInfo.Local.DaylightName);
-        Console.WriteLine("BaseUtcOffset:\t" + TimeZoneInfo.Local.BaseUtcOffset);
+        Console.WriteLine($"Id:\t\t{TimeZoneInfo.Local.Id}");
+        Console.WriteLine($"Display Name:\t{TimeZoneInfo.Local.DisplayName}");
+        Console.WriteLine($"StandardName:\t{TimeZoneInfo.Local.StandardName}");
+        Console.WriteLine($"DaylightName:\t{TimeZoneInfo.Local.DaylightName}");
+        Console.WriteLine($"BaseUtcOffset:\t{TimeZoneInfo.Local.BaseUtcOffset}");
         Console.WriteLine();
 
         Console.WriteLine("Now\n===");
@@ -22,9 +22,9 @@ class Program
     static void Display(DateTime dt)
     {
         Console.WriteLine(dt.ToString());
-        Console.WriteLine("Kind:\t" + dt.Kind);
-        Console.WriteLine("UTC:\t" + dt.ToUniversalTime().ToString());
-        Console.WriteLine("Local:\t" + dt.ToLocalTime().ToString());
+        Console.WriteLine($"Kind:\t{dt.Kind}");
+        Console.WriteLine($"UTC:\t{dt.ToUniversalTime().ToString()}");
+        Console.WriteLine($"Local:\t{dt.ToLocalTime().ToString()}");
         Console.WriteLine();
     }
 }
